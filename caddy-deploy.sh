0c4570111b93c4c84311ae27955d01c1bec45e40
Ross Reicks <rreicks13@gmail.com>
2025-06-08 10:35:35 -0500

    fix: caddy path

--- caddy-deploy.sh
+++ caddy-deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Simple script to pull the latest Caddyfile and reload Caddy
-# Run from the root of the repo
-# need to chmod +x this file first
+# to setup, run:
+# sudo mkdir -p /opt/caddy
+# sudo chown $USER /opt/caddy
+# cd /opt/caddy
+# git clone https://github.com/RossBossDev/home-gitops .
 set -e
 
-CADDYFILE=/etc/caddy/Caddyfile
+CADDYFILE=/opt/caddy/Caddyfile
 
 echo "[1/3] Validating Caddyfile..."
 caddy validate --config "$CADDYFILE"
